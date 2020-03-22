551f55ddde56cf243166e89db9ad98c2a1442f17
linzexin <linzexinmasterchief@gmail.com>
2020-03-22 10:12:56 +0800

    add checkpoint

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -7,10 +7,15 @@ public class CharacterControl : MonoBehaviour
     public float speed;
     public Vector3 jump_vector;
 
+    private Vector3 start_pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        start_pos = gameObject.transform.position;
+#if UNITY_EDITOR
+        PlayerPrefs.DeleteAll();
+#endif
     }
 
     // Update is called once per frame
@@ -21,14 +26,22 @@ public class CharacterControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // reload checkpoint
+        if (Input.GetKey(KeyCode.R))
+        {
+            float pos_x = PlayerPrefs.GetFloat("pos_x", start_pos.x);
+            float pos_y = PlayerPrefs.GetFloat("pos_y", start_pos.y);
+            gameObject.transform.position = new Vector3(pos_x, pos_y, 0);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, gameObject.GetComponent<Rigidbody>().mass * speed / 3));
+            gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, gameObject.GetComponent<Rigidbody>().mass * speed));
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, -gameObject.GetComponent<Rigidbody>().mass * speed / 3));
+            gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, -gameObject.GetComponent<Rigidbody>().mass * speed));
         }
 
         if (Input.GetKey(KeyCode.Space))
---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Material triggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerPrefs.SetFloat("pos_x", gameObject.transform.position.x);
+        PlayerPrefs.SetFloat("pos_y", gameObject.transform.position.y);
+        gameObject.GetComponent<MeshRenderer>().material = triggered;
+    }
+
+}
