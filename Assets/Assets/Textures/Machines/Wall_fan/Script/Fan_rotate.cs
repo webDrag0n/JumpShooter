5f5e08ff87999d60d69cb87db9df4bc958d31a6e
linzexin <linzexinmasterchief@gmail.com>
2020-08-08 13:06:40 +0800

    add wall fan

---
+++ Assets/Assets/Textures/Machines/Wall_fan/Script/Fan_rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan_rotate : MonoBehaviour
+{
+
+    public float rotation_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, rotation_speed));
+    }
+}
